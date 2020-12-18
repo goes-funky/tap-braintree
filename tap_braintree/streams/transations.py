ba58a95fed0eaf490b6244e48db54da0bbf98e66
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2020-12-18 17:43:24 +0100

    adding braintree backoff

--- tap_braintree/streams/transations.py
+++ tap_braintree/streams/transations.py
@@ -2,16 +2,20 @@ import braintree
 
 from tap_braintree.context import Context
 from tap_braintree.streams.base import Stream
+from braintree.exceptions import UnexpectedError
+from .base import braintree_error_handling
+
+RAISE_ERROR = True
 
 
 class TransactionsStream(Stream):
     name = 'transactions'
     replication_key = "created_at"
     @staticmethod
+    @braintree_error_handling
     def stream_data(start, end):
         data = braintree.Transaction.search(
             braintree.TransactionSearch.created_at.between(start, end))
-
         if len(data.ids) == 50000:
             raise Exception("Got more than 50000 transactions")
         return data
